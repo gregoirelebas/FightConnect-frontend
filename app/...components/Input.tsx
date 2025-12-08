10efbc578cd86f24a0dffab2e8e78a892c504c67
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-08 14:32:10 +0100

    Created Input component.

---
+++ app/...components/Input.tsx
@@ -0,0 +1,32 @@
+interface InputProps {
+  label: string;
+  placeholder: string;
+  type?: string;
+  value: string | undefined;
+  onChange: (value: string) => void;
+  onValidate?: () => void;
+}
+
+export default function Input(props: InputProps) {
+  const handleKeyDown = (key: string) => {
+    if (key === 'Enter' && props.onValidate) {
+      props.onValidate();
+    }
+  };
+
+  return (
+    <div className="flex-col items-center">
+      <div>
+        <span className="text-sm">{props.label}</span>
+      </div>
+      <input
+        className="bg-background border text-md border-grey mt-1 px-4 py-3 rounded-md focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent transition-all placeholder:text-grey"
+        placeholder={props.placeholder}
+        value={props.value}
+        type={props.type ? props.type : 'text'}
+        onChange={(e) => props.onChange(e.target.value)}
+        onKeyDownCapture={(e) => handleKeyDown(e.key)}
+      />
+    </div>
+  );
+}
--- app/toolkit/page.tsx
+++ app/toolkit/page.tsx
@@ -1,12 +1,37 @@
+'use client';
+
+import { useState } from 'react';
 import Button, { ButtonVariant } from '../...components/Button';
+import Input from '../...components/Input';
 
 export default function Toolkit() {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
+  };
+
+  const handleEnter = () => {
+    console.log('Enter!');
+  };
+
   return (
-    <div className="flex gap-2 w-full h-full mt-5 ml-5">
-      <Button className="w-[400]">Primary</Button>
-      <Button variant={ButtonVariant.Secondary}>Secondary</Button>
-      <Button variant={ButtonVariant.Accept}>Accept</Button>
-      <Button variant={ButtonVariant.Refuse}>Refuse</Button>
+    <div className="flex-col mt-10 gap-y-2">
+      <div className="flex justify-center gap-20">
+        <Button className="w-[400]">Primary</Button>
+        <Button variant={ButtonVariant.Secondary}>Secondary</Button>
+        <Button variant={ButtonVariant.Accept}>Accept</Button>
+        <Button variant={ButtonVariant.Refuse}>Refuse</Button>
+      </div>
+      <div className="ml-5">
+        <Input
+          label="Input field"
+          placeholder="Placeholder value"
+          value={inputValue}
+          onChange={handleInputChange}
+          onValidate={handleEnter}
+        />
+      </div>
     </div>
   );
 }
