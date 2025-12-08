dad3f9385a1561ff199c1075cac773fbf71695b6
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-08 14:37:29 +0100

    Added missing className to Input component.

--- app/...components/Input.tsx
+++ app/...components/Input.tsx
@@ -1,6 +1,7 @@
 interface InputProps {
   label: string;
   placeholder: string;
+  className?: string;
   type?: string;
   value: string | undefined;
   onChange: (value: string) => void;
@@ -20,7 +21,9 @@ export default function Input(props: InputProps) {
         <span className="text-sm">{props.label}</span>
       </div>
       <input
-        className="bg-background border text-md border-grey mt-1 px-4 py-3 rounded-md focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent transition-all placeholder:text-grey"
+        className={`${
+          props.className ? props.className : ''
+        } bg-background border text-md border-grey mt-1 px-4 py-3 rounded-md focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent transition-all placeholder:text-grey`}
         placeholder={props.placeholder}
         value={props.value}
         type={props.type ? props.type : 'text'}
