1c1e935c02d1d79fb0f7734386f922e9570fdd33
sylvain.b <sylvain.berthere@gmail.com>
2025-12-09 17:38:48 +0100

    Changed the bg of header to be the same of bg body

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const header = () => {
   return (
-      <div className="header text w-full h-20 pl-20 bg-black border-b-red-900 border-l-0 border-r-0 border-t-0 border flex flex-row justify-between items-center">
+      <div className="header text w-full h-20 pl-20 bg-background border-b-red-900 border-l-0 border-r-0 border-t-0 border flex flex-row justify-between items-center">
         <h1 className="text-4xl">Fight Connect</h1>
         <nav >
           <ul className="flex flex-row text-xl w-150 justify-around">
