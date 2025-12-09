da211d913c158f126a0ee6bb454444a1fd7e0db6
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-09 16:25:41 +0100

    Fixed some errors in login page.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,28 +15,25 @@ export default function Login() {
     <div className="flex min-h-screen justify-center bg-[url(/LandingFond.jpg)] bg-cover font-sans ">
       <div className="h-225 w-200 pt-10 flex flex-col justify-around items-center">
         <h1 className="text-white text-7xl font-bold">Fight Connect</h1>
-        <text className="text">
+        <h3 className="text-center">
           Less headaches, more fights! Find fighters or fighting events fast and safely...
-        </text>
-        <div className="mb-40 h-80 w-100 flex flex-col justify-around items-center rounded-lg bg-gray-950/95">
+        </h3>
+        <div className="mb-40 h-80 w-100 flex flex-col justify-around items-center rounded-lg bg-background">
           <Input
-            className="h-10 w-70"
+            className="w-xs"
             label={'Username'}
-            placeholder={'Enter Username'}
+            placeholder={'Username...'}
             value={username}
             onChange={setUsername}></Input>
           <Input
-            className="h-10 w-70"
+            className="w-xs"
             label={'Password'}
-            placeholder={'Enter password'}
+            placeholder={'Password...'}
             value={password}
             onChange={setPassword}
             type="password"></Input>
           <Link href={'/events'}>
-            <Button
-              variant={ButtonVariant.Primary}
-              onClick={() => handleLogin()}
-              className="h-10 w-70">
+            <Button variant={ButtonVariant.Primary} onClick={() => handleLogin()} className="w-xs">
               Login
             </Button>
           </Link>
