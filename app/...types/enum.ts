aa7d34fbfc076273f321a99eda65a3e3cce56b52
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-09 16:44:56 +0100

    Created a custom enum helper.

---
+++ app/...types/enum.ts
@@ -0,0 +1,27 @@
+enum Sport {
+  MMA = 'mma',
+  Jiujitsu = 'jjb',
+  KickBoxing = 'kickBoxing',
+  MuayThai = 'muaythai',
+  EnglishBoxing = 'englishBoxing',
+}
+
+enum Level {
+  Pro = 'pro',
+  Amateur = 'amateur',
+}
+
+enum Experience {
+  Zero = '0',
+  OneThree = '1-3',
+  FourSix = '4-6',
+  SevenNine = '7-9',
+  TenTwelve = '10-12',
+  ThirteenFifteen = '13-15',
+  SixteenEighteen = '16-18',
+  NineteenTwentyOne = '19-21',
+  TwentyTwoTwentyFour = '22-24',
+  TwentyFivePlus = '25+',
+}
+
+export { Sport, Level, Experience };
