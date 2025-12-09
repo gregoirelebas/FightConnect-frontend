a8355136674def8f264f24019c70d5e4a12598cc
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-09 15:26:06 +0100

    Modified Button component to add a Ternary variant.

--- app/...components/Button.tsx
+++ app/...components/Button.tsx
@@ -3,14 +3,15 @@ import { ReactNode } from 'react';
 export enum ButtonVariant {
   Primary,
   Secondary,
+  Ternary,
   Accept,
   Refuse,
 }
 
 interface ButtonProps {
-  variant?: ButtonVariant;
+  variant: ButtonVariant;
   className?: string;
-  children?: ReactNode;
+  children: ReactNode;
   onClick?: () => void;
 }
 
@@ -26,22 +27,18 @@ interface ButtonProps {
  * ```
  *
  * @param {ButtonProps} props - The button component props
- * @param {ButtonVariant} [props.variant=ButtonVariant.Primary] - The visual variant of the button (Primary, Secondary, Accept, or Refuse)
- * @param {string} [props.className=''] - Additional CSS classes to apply to the button
+ * @param {ButtonVariant} [props.variant] - The visual variant of the button (Primary, Secondary, Ternary, Accept, or Refuse)
+ * @param {string} [props.className] - Additional CSS classes to apply to the button
  * @param {React.ReactNode} props.children - The content to display inside the button
  * @param {React.MouseEventHandler<HTMLButtonElement>} props.onClick - Callback function triggered when the button is clicked
  * @returns {JSX.Element} A styled button element with variant-specific colors and hover/active states
  */
-export default function Button({
-  variant = ButtonVariant.Primary,
-  className = '',
-  children,
-  onClick,
-}: ButtonProps) {
-  let backgroundColor: string = 'bg-primary';
-  let hoverColor: string = 'hover:bg-primary-hover';
+export default function Button(props: ButtonProps) {
+  let backgroundColor: string;
+  let hoverColor: string;
+  let border: string = 'none';
 
-  switch (variant) {
+  switch (props.variant) {
     case ButtonVariant.Primary:
       backgroundColor = 'bg-primary';
       hoverColor = 'hover:bg-primary-hover';
@@ -50,6 +47,12 @@ export default function Button({
     case ButtonVariant.Secondary:
       backgroundColor = 'bg-accent';
       hoverColor = 'hover:bg-accent-hover';
+      break;
+
+    case ButtonVariant.Ternary:
+      backgroundColor = 'bg-background';
+      hoverColor = 'hover:bg-accent-hover';
+      border = 'border border-accent';
       break;
 
     case ButtonVariant.Accept:
@@ -65,9 +68,9 @@ export default function Button({
 
   return (
     <button
-      className={`${className} ${backgroundColor} px-6 py-3 rounded-lg transition-all duration-200 font-medium text-white ${hoverColor} active:scale-95 cursor-pointer`}
-      onClick={onClick}>
-      {children}
+      className={`${props.className} ${backgroundColor} ${border} px-6 py-3 rounded-lg transition-all duration-200 font-medium text-white ${hoverColor} active:scale-95 cursor-pointer`}
+      onClick={props.onClick}>
+      {props.children}
     </button>
   );
 }
