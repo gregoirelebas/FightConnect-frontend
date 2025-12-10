55b1aade99fb8d9fce51c1b3ac0e1a778af85e14
sylvain.b <sylvain.berthere@gmail.com>
2025-12-10 11:37:57 +0100

    Added fetch with backend for login and go to page events search

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,46 +1,69 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Button, { ButtonVariant } from '../...components/Button';
-import Link from 'next/link';
-import Input from '../...components/Input';
+import { useState } from "react";
+import Button, { ButtonVariant } from "../...components/Button";
+import Link from "next/link";
+import Input from "../...components/Input";
+import { useRouter } from "next/navigation";
+
 
 export default function Login() {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  
+  const router = useRouter(); 
 
-  const handleLogin = () => {};
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = () => {
+
+    fetch("http://10.0.3.177:3000/users/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({name,password}),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.result===true) {
+        router.push("/events")
+      }
+    })
+
+  };
 
   return (
     <div className="flex min-h-screen justify-center bg-[url(/LandingFond.jpg)] bg-cover font-sans ">
       <div className="h-225 w-250 pt-7 flex flex-col justify-around items-center">
-         <h1 className="text-white pt-3 ml-65 w-180 text-7xl font-bold">Fight Connect</h1>
-        
-        <h3 className="pb-2">Less headaches, more fights! Find fighters or fighting events fast and safely...</h3>
-        <div className="mb-47 h-80 w-100 flex flex-col justify-around items-center rounded-lg bg-background">
-          <Link href={'/'}>
-          <text className="font-bold text-xl ml-85">X</text>
+        <h1 className="text-white pt-3 ml-65 w-180 text-7xl font-bold">Fight Connect</h1>
+
+        <h3 className="pb-2 mb-2">
+          Less headaches, more fights! Find fighters or fighting events fast and safely...
+        </h3>
+        <div className="mb-45 h-80 w-100 flex flex-col justify-around items-center rounded-lg bg-background">
+          <Link href={"/"}>
+            <p className="font-bold text-xl ml-85">X</p>
           </Link>
           <Input
             className="w-xs"
-            label={'Username'}
-            placeholder={'Username...'}
-            value={username}
-            onChange={setUsername}></Input>
+            label={"Username"}
+            placeholder={"Username..."}
+            value={name}
+            onChange={value => setName(String(value))}
+          ></Input>
           <Input
             className="w-xs"
-            label={'Password'}
-            placeholder={'Password...'}
+            label={"Password"}
+            placeholder={"Password..."}
             value={password}
-            onChange={setPassword}
-            type="password"></Input>
-          <Link href={'/events'}>
-            <Button variant={ButtonVariant.Primary} onClick={() => handleLogin()} className="w-xs">
-              Login
-            </Button>
-          </Link>
+            onChange={value => setPassword(String(value))}
+            type="password"
+          ></Input>
+          <Button variant={ButtonVariant.Primary} onClick={() => handleLogin()} className="w-xs">
+            Login
+          </Button>
         </div>
       </div>
     </div>
   );
 }
+
+
