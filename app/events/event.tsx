bb9b2b17d93bdd680c1a2719f9cbd3613e9052f2
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-12 16:20:39 +0100

    Improved events display page

--- app/events/event.tsx
+++ app/events/event.tsx
@@ -1,43 +1,47 @@
-import Button, { ButtonVariant } from "../...components/Button";
+import Button, { ButtonVariant } from '../...components/Button';
 
 export default function Event(props: {
+  token: string;
   name: string;
   date: string;
   sport: string;
   experience: string;
   weight: string;
   level: string;
-  id: string;
   setIsPopUp: React.Dispatch<React.SetStateAction<boolean>>;
   setCurrentEvent: React.Dispatch<React.SetStateAction<string>>;
-
 }) {
   const date = new Date(props.date);
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
 
-  const structuredDate = `${year}-${month}-${day}`
+  const structuredDate = `${year}-${month}-${day}`;
 
-  const handlePopUp =()=>{
+  const handlePopUp = () => {
     props.setIsPopUp(true);
-    props.setCurrentEvent(props.id);
-  }
+    props.setCurrentEvent(props.token);
+  };
 
   return (
-    <div className="w-69 h-79 flex flex-col justify-around items-center m-3 font-extrabold bg-[url(/boxing.png)] bg-cover rounded-3xl">
+    <div className="  h-80 flex flex-col justify-around items-center m-3 font-extrabold bg-[url(/boxing.png)] bg-cover bg-center rounded-3xl">
       <div className="flex flex-col items-center">
         <h3>{props.name}</h3>
         <span>{structuredDate}</span>
       </div>
       <span>Sport : {props.sport}</span>
-      <span>Expérience : {props.experience}</span>
+      <span>Experience : {props.experience}</span>
       <div className="flex flex-row w-60 justify-around">
         <span>Weight : {props.weight}</span>
         <span>-</span>
         <span>Level : {props.level}</span>
       </div>
-      <Button onClick={handlePopUp} variant={ButtonVariant.Primary} className="h-10 w-40 flex justify-center items-center">More info</Button>
+      <Button
+        onClick={handlePopUp}
+        variant={ButtonVariant.Primary}
+        className="h-10 w-40 flex justify-center items-center">
+        More info
+      </Button>
     </div>
   );
 }
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,39 +1,53 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import Input from "../...components/Input";
-import Button, { ButtonVariant } from "../...components/Button";
-import Event from "./event";
-import RadioButton from "../...components/RadioButton";
-import Dropdown from "../...components/Dropdown";
-import { Sport, Level, Experience, Weight } from "../...types/enum";
-import PopUpEvent from "./PopUpEvent";
+'use client';
 
-export default function Events() {
-  const [search, setSearch] = useState<string>("");
+import { useEffect, useState } from 'react';
+import Input from '../...components/Input';
+import Button, { ButtonVariant } from '../...components/Button';
+import type { Event } from '../...types/event';
+import EventComponent from './event';
+import RadioButton from '../...components/RadioButton';
+import Dropdown from '../...components/Dropdown';
+import { Sport, Level, Experience, Weight } from '../...types/enum';
+import PopUpEvent from './PopUpEvent';
+
+export default function EventsPage() {
+  const [search, setSearch] = useState<string>('');
   const [level, setLevel] = useState<Level>(Level.Amateur);
-  const [sport, setSport] = useState<Sport[]>([]);
+  const [sport, setSport] = useState<Sport>(Sport.EnglishBoxing);
   const [experience, setExperience] = useState<Experience>(Experience.Zero);
-  const [weight, setWeight] = useState<Weight>(Weight.FiftyTwoFiftySeven)
-  const [allEvents, setAllEvents] = useState([])
+  const [weight, setWeight] = useState<Weight>(Weight.FiftyTwoFiftySeven);
+  const [allEvents, setAllEvents] = useState([]);
   const [isPopUp, setIsPopUp] = useState(false);
   const [currentEvent, setCurrentEvent] = useState('');
 
   useEffect(() => {
-    const result = fetch(process.env.NEXT_PUBLIC_API_URL + "events/search")
+    fetch(process.env.NEXT_PUBLIC_API_URL + 'events/search')
       .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          setAllEvents(data.event);
+      .then((request) => {
+        if (request.result) {
+          setAllEvents(request.data);
         } else {
-          console.error("Error registering fighter:", data.error);
+          console.error('Error registering fighter:', request.error);
         }
       });
   }, []);
 
-  const cardEvent = allEvents.map((data: any, i) => {
-    return <Event key={i} id={data._id} setCurrentEvent={setCurrentEvent} setIsPopUp={setIsPopUp} name={data.name} date={data.date} sport={data.sports} experience={data.experience} weight={data.weight} level={data.level} />
-  })
-
+  const cardEvents = allEvents.map((data: Event, i) => {
+    return (
+      <EventComponent
+        key={i}
+        token={data.token}
+        name={data.name}
+        date={data.date}
+        sport={data.sport}
+        experience={data.experience}
+        weight={data.weight}
+        level={data.level}
+        setCurrentEvent={setCurrentEvent}
+        setIsPopUp={setIsPopUp}
+      />
+    );
+  });
 
   return (
     <>
@@ -42,15 +56,14 @@ export default function Events() {
           <div className="w-1/5"></div>
           <Input
             className="w-200 h-10"
-            label={"You can search here :"}
-            placeholder={"Search an event"}
+            label={'You can search here :'}
+            placeholder={'Search an event'}
             value={search}
             onChange={(value) => setSearch(String(value))}
-          ></Input>
+          />
           <Button
             variant={ButtonVariant.Primary}
-            className="h-10 w-40 ml-80 mt-8 flex justify-center items-center"
-          >
+            className="h-10 w-40 ml-80 mt-8 flex justify-center items-center">
             Search
           </Button>
         </div>
@@ -65,77 +78,76 @@ export default function Events() {
                   label="Pro"
                   value={Level.Pro}
                   onChange={(value) => setLevel(value as Level)}
-                ></RadioButton>
+                />
                 <RadioButton
                   name="level"
                   label="Amateur"
                   value={Level.Amateur}
                   onChange={(value) => setLevel(value as Level)}
-                ></RadioButton>
+                />
               </div>
             </div>
-          <div className="h-20 mt-8 flex flex-col justify-between">
-            <span>Sports :</span>
-            <Dropdown
-              className="w-50"
-              options={[
-                { value: Sport.MMA, label: "MMA" },
-                { value: Sport.KickBoxing, label: "Kick Boxing" },
-                { value: Sport.Jiujitsu, label: "Jiu Jitsu" },
-                { value: Sport.MuayThai, label: "Muay Thai" },
-                { value: Sport.EnglishBoxing, label: "English Boxing" },
-              ]}
-              onChange={(value) => setSport([value as Sport])}
-            ></Dropdown>
+            <div className="h-20 mt-8 flex flex-col justify-between">
+              <span>Sports :</span>
+              <Dropdown
+                className="w-50"
+                options={[
+                  { value: Sport.MMA, label: 'MMA' },
+                  { value: Sport.KickBoxing, label: 'Kick Boxing' },
+                  { value: Sport.Jiujitsu, label: 'Jiu Jitsu' },
+                  { value: Sport.MuayThai, label: 'Muay Thai' },
+                  { value: Sport.EnglishBoxing, label: 'English Boxing' },
+                ]}
+                onChange={(value) => setSport(value as Sport)}
+              />
+            </div>
+            <div className="h-20 mt-8 flex flex-col justify-between">
+              <span>Experience :</span>
+              <Dropdown
+                className="w-50"
+                options={[
+                  { value: Experience.Zero, label: '0' },
+                  { value: Experience.OneThree, label: '1-3' },
+                  { value: Experience.FourSix, label: '4-6' },
+                  { value: Experience.SevenNine, label: '7-9' },
+                  { value: Experience.TenTwelve, label: '10-12' },
+                  { value: Experience.ThirteenFifteen, label: '13-15' },
+                  { value: Experience.SixteenEighteen, label: '16-18' },
+                  { value: Experience.NineteenTwentyOne, label: '19-21' },
+                  { value: Experience.TwentyTwoTwentyFour, label: '22-24' },
+                  { value: Experience.TwentyFivePlus, label: '25+' },
+                ]}
+                onChange={(value) => setExperience(value as Experience)}
+              />
+            </div>
+            <div className="h-20 flex flex-col justify-between">
+              <span>Weight :</span>
+              <Dropdown
+                className="w-50"
+                options={[
+                  { value: Weight.FiftyTwoFiftySeven, label: '52-57' },
+                  { value: Weight.FiftySevenSixtyOne, label: '57-61' },
+                  { value: Weight.SixtyOneSixtySix, label: '61-66' },
+                  { value: Weight.SixtySixSeventy, label: '66-70' },
+                  { value: Weight.SeventySeventySeven, label: '70-77' },
+                  { value: Weight.SeventySevenEightyFour, label: '77-84' },
+                  { value: Weight.EightyFourNinetyThree, label: '84-93' },
+                  { value: Weight.NinetyThreeOneHundredTwenty, label: '93-120' },
+                ]}
+                onChange={(value) => setWeight(value as Weight)}
+              />
+            </div>
+            <Button
+              variant={ButtonVariant.Primary}
+              className="h-10 w-40 mt-10 flex justify-center items-center">
+              Apply
+            </Button>
           </div>
-          <div className="h-20 mt-8 flex flex-col justify-between">
-            <span>Experience :</span>
-            <Dropdown
-              className="w-50"
-              options={[
-                { value: Experience.Zero, label: "0" },
-                { value: Experience.OneThree, label: "1-3" },
-                { value: Experience.FourSix, label: "4-6" },
-                { value: Experience.SevenNine, label: "7-9" },
-                { value: Experience.TenTwelve, label: "10-12" },
-                { value: Experience.ThirteenFifteen, label: "13-15" },
-                { value: Experience.SixteenEighteen, label: "16-18" },
-                { value: Experience.NineteenTwentyOne, label: "19-21" },
-                { value: Experience.TwentyTwoTwentyFour, label: "22-24" },
-                { value: Experience.TwentyFivePlus, label: "25+" },
-              ]}
-              onChange={(value) => setExperience(value as Experience)}
-            ></Dropdown>
+          <div className="w-4/5 pl-3 mb-5 ml-1 mr-2 mt-2 flex flex-col border border-gray-600 rounded-3xl overflow-y-auto scrollb">
+            <h3 className="w-full text-center">Events</h3>
+            <div className="grid grid-cols-5">{cardEvents}</div>
           </div>
-          <div className="h-20 flex flex-col justify-between">
-            <span>Weight :</span>
-            <Dropdown
-              className="w-50"
-              options={[
-                { value: Weight.FiftyTwoFiftySeven, label: "52-57" },
-                { value: Weight.FiftySevenSixtyOne, label: "57-61" },
-                { value: Weight.SixtyOneSixtySix, label: "61-66" },
-                { value: Weight.SixtySixSeventy, label: "66-70" },
-                { value: Weight.SeventySeventySeven, label: "70-77" },
-                { value: Weight.SeventySevenEightyFour, label: "77-84" },
-                { value: Weight.EightyFourNinetyThree, label: "84-93" },
-                { value: Weight.NinetyThreeOneHundredTwenty, label: "93-120" },
-              ]}
-              onChange={(value) => setWeight(value as Weight)}
-            ></Dropdown>
-          </div>
-          <Button
-            variant={ButtonVariant.Primary}
-            className="h-10 w-40 mt-10 flex justify-center items-center"
-          >
-            Apply
-          </Button>
-          </div>
-        <div className="w-4/5 pl-3 mb-5 ml-1 mr-2 mt-2 flex flex-col border border-gray-600 rounded-3xl">
-          <h3 className="w-full">Events</h3>
-          <div className="flex flex-wrap">{cardEvent}</div>
         </div>
-      </div>
       </div>
       {isPopUp && <PopUpEvent setIsPopUp={setIsPopUp} id={currentEvent} />}
     </>
