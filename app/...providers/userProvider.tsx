4c77d011b30542a6caaeafb835351a173b46b79d
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-12 14:21:49 +0100

    Set up user provider.

--- app/...providers/userProvider.tsx
+++ app/...providers/userProvider.tsx
@@ -1,18 +1,34 @@
 'use client';
 
 import React, { createContext, Dispatch, SetStateAction, useState } from 'react';
+import { Role } from '../...types/enum';
 
 interface User {
   token: string;
   setToken: Dispatch<SetStateAction<string>>;
+
+  userID: string;
+  setUserID: Dispatch<SetStateAction<string>>;
+
+  role: Role;
+  setRole: Dispatch<SetStateAction<Role>>;
 }
 
 export const UserContext = createContext<User>({} as User);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [token, setToken] = useState<string>('');
+  const [userID, setUserID] = useState<string>('');
+  const [role, setRole] = useState<Role>(Role.Fighter);
 
-  const user: User = { setToken, token };
+  const user: User = {
+    token: token,
+    userID: userID,
+    role: role,
+    setToken: setToken,
+    setUserID: setUserID,
+    setRole: setRole,
+  };
 
   return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
