9b45e2c84ed871033d7cec1d1d4c9c05445a9b07
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-12 10:40:16 +0100

    Created a separate SportDropdown component.

---
+++ app/...UI/SportDropdown.tsx
@@ -0,0 +1,18 @@
+import Dropdown, { DropdownOptionProps } from '../...components/Dropdown';
+import { Sport } from '../...types/enum';
+
+interface SportDropdownProps {
+  onChange: (value: string) => void;
+}
+
+export default function SportDropdown(props: SportDropdownProps) {
+  const sportOptions: DropdownOptionProps[] = [
+    { label: 'English boxing', value: Sport.EnglishBoxing },
+    { label: 'Brasilian Jiu-jitsu', value: Sport.Jiujitsu },
+    { label: 'Kickboxing', value: Sport.KickBoxing },
+    { label: 'MMA', value: Sport.MMA },
+    { label: 'Muay Thai', value: Sport.MuayThai },
+  ];
+
+  return <Dropdown options={sportOptions} onChange={props.onChange} />;
+}
--- app/signup/promoter/signupPromoter.tsx
+++ app/signup/promoter/signupPromoter.tsx
@@ -1,12 +1,15 @@
+'use client';
+
 import { Role } from '@/app/...types/enum';
 import RoleSwitch from '../roleSwitch';
+import SportDropdown from '@/app/...UI/SportDropdown';
 
 export default function SignupPromoterComponent() {
   return (
     <div className="flex flex-col my-10 mx-20">
       <RoleSwitch role={Role.Promoter} />
       <h3>Sport</h3>
-      <SportDropdown />
+      <SportDropdown onChange={() => {}} />
     </div>
   );
 }
