980d1a932f367a8524ae7a95486b5e33f92f78ad
Gregoire Lebas <gregoire.lebas@gmail.com>
2025-12-16 13:00:13 +0100

    Created the core layout of event infos.

--- app/events/[token]/...UI/eventInfos.tsx
+++ app/events/[token]/...UI/eventInfos.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Button, { ButtonVariant } from '@/app/...components/Button';
 import { dateToString } from '@/app/...helpers/date';
 import { setCookieState } from '@/app/...helpers/states';
 import Cookies from '@/app/...types/cookies';
 import { getCookie } from '@/app/...helpers/cookies';
 import { Event } from '@/app/...types/event';
+import { LevelToString, SportToString } from '@/app/...helpers/enum';
+
+import FighterApplicant from './fighterApplicant';
+import { Fighter } from '@/app/...types/fighter';
 
 export default function EventInfos({ token }: { token: string | undefined }) {
   const [userToken, setUserToken] = useState<string | undefined>(undefined);
   const [event, setEvent] = useState<Event>();
+  const [fighters, setFighters] = useState<Fighter[]>([]);
 
   useEffect(() => {
     async function fetchEvent() {
@@ -34,64 +38,53 @@ export default function EventInfos({ token }: { token: string | undefined }) {
     fetchEvent();
   }, [token]);
 
-  return (
-    (!event && <div>Loading...</div>) ||
-    (event && (
-      <div className="absolute top-0 left-0 w-screen h-screen bg-background/80 flex justify-center items-center">
-        <div className="w-1/2 h-2/3 bg-white flex justify-center items-center flex-col">
-          <div className="h-1/10 w-full bg-gray-300 flex justify-around items-center">
-            <h3 className="text-black pl-65 font-bold flex justify-center">Event : {event.name}</h3>
-            <p className="pl-30 text-2xl font-bold text-black">X</p>
-          </div>
-          <div className="h-9/10 w-full border-2 border-gray-950 border-l-0 border-r-0 border-b-0 ">
-            <div className="h-1/3 w-full bg-white text-black flex flex-col justify-around items-center">
-              <div className="w-full flex flex-row justify-around">
-                <p>{event.name}</p>
-                <p>{dateToString(event.date)}</p>
-                <p>{event.level}</p>
-              </div>
-              <span>{event.description}</span>
-            </div>
-            <div className="h-2/3 w-full pt-10 bg-gray-300 border-black border-2 border-l-0 border-r-0 border-b-0 flex flex-col items-center overflow-y-auto">
-              {/* Fighter div for accept ou refuse  */}
-              <div className="min-h-10 w-9/10 mt-3 bg-foreground text-white flex justify-around items-center ">
-                <span>Fighter Name</span>
-                <span>Date</span>
-                <span>Catégorie</span>
-                <div className="flex flex-row w-40 justify-around">
-                  <Button
-                    variant={ButtonVariant.Accept}
-                    className="w-15 h-8 text-xs flex justify-center items-center">
-                    Accept
-                  </Button>
-                  <Button
-                    variant={ButtonVariant.Refuse}
-                    className="w-15 h-8 text-xs flex justify-center items-center">
-                    Refuse
-                  </Button>
-                </div>
-              </div>
-              <div className="min-h-10 w-9/10 mt-3 bg-foreground text-white flex justify-around items-center overflow-y-auto">
-                <span>Fighter Name</span>
-                <span>Date</span>
-                <span>Catégorie</span>
-                <div className="flex flex-row w-40 justify-around">
-                  <Button
-                    variant={ButtonVariant.Accept}
-                    className="w-15 h-8 text-xs flex justify-center items-center">
-                    Accept
-                  </Button>
-                  <Button
-                    variant={ButtonVariant.Refuse}
-                    className="w-15 h-8 text-xs flex justify-center items-center">
-                    Refuse
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
+  function createInfoCard(text: string, data: string, color: string) {
+    return (
+      <div className={`h-30 pl-2 rounded-2xl ${color}`}>
+        <div className="h-full flex flex-col p-5 rounded-l-none rounded-xl gap-2 bg-foreground">
+          <span className="text-xl">{text}</span>
+          <span>{data}</span>
         </div>
       </div>
-    ))
+    );
+  }
+
+  const applicants = fighters.map((fighter: Fighter, i: number) => {
+    return <FighterApplicant key={i} fighter={fighter} />;
+  });
+
+  return (
+    event && (
+      <div className="flex flex-col mx-100 my-10 gap-10">
+        <div className="h-80 card justify-end bg-[url('@/public/LandingFond.jpg')] bg-cover bg-center">
+          <div className="flex gap-5">
+            <span className="pill">{LevelToString(event.level)}</span>
+            <span className="pill">{SportToString(event.sport)}</span>
+          </div>
+          <h1>{event.name}</h1>
+          <span className="text-grey">{event.clubName}</span>
+        </div>
+        <div className="grid grid-cols-4 gap-5">
+          {createInfoCard('Date', dateToString(event.date), 'bg-accent')}
+          {createInfoCard('Location', 'Event city', 'bg-error')}
+          {createInfoCard('Experience', `${event.experience} fights`, 'bg-warning')}
+          {createInfoCard('Weight class', `${event.weight} kg`, 'bg-success')}
+        </div>
+        <div className="card">
+          <h3>Description</h3>
+          <p className="text-grey">{event.description}</p>
+        </div>
+        <div>
+          <h2 className="font-bold mb-5">Fighter Applicants</h2>
+          {fighters.length == 0 ? (
+            <div className="card">
+              <h3>No applicants yet</h3>
+            </div>
+          ) : (
+            <div className="grid grid-cols-3 scroll-my-0">{applicants}</div>
+          )}
+        </div>
+      </div>
+    )
   );
 }
---
+++ app/events/[token]/...UI/fighterApplicant.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import { Fighter } from '@/app/...types/fighter';
+
+import Button, { ButtonVariant } from '@/app/...components/Button';
+import profile from '@/public/defaultProfile.png';
+import { useRouter } from 'next/navigation';
+
+export default function FighterApplicant({ fighter }: { fighter: Fighter }) {
+  const router = useRouter();
+
+  function viewProfile() {
+    router.push('/profile/' + fighter.name);
+  }
+
+  return (
+    <div className="card items-center">
+      <Image src={profile} alt={'Profile picture'} width={150} height={0} />
+      <h3>{fighter.name}</h3>
+      <div className="card flex-row items-center bg-background text-xl">
+        <div className="flex items-center gap-2">
+          <span className="text-success">{fighter.victoryCount}</span>•
+          <span className="text-error">{fighter.defeatCount}</span>•
+          <span className="text-warning">{fighter.drawCount}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span>{fighter.weight}kg</span>
+          <span>•</span>
+          <span>{fighter.height}cm</span>
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-5">
+        <Button variant={ButtonVariant.Ternary} onClick={viewProfile}>
+          View profile
+        </Button>
+        <div className="grid grid-cols-2 gap-5">
+          <Button variant={ButtonVariant.Accept}>Accept</Button>
+          <Button variant={ButtonVariant.Refuse}>Refuse</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
