5ac86dfe6b9277ff0c82957d6a8641c111717860
kalifa8869 <kalifa.seydi@live.fr>
2025-12-16 15:06:09 +0100

    add event

--- app/events/PopUpEvent.tsx
+++ app/events/PopUpEvent.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react';
-import Button, { ButtonVariant } from '../...components/Button';
-import { Event } from '../...types/event';
-import { dateToString } from '../...helpers/date';
+import { useEffect, useState } from "react";
+import Button, { ButtonVariant } from "../...components/Button";
+import { Event } from "../...types/event";
+import { DateToString } from "../...helpers/date";
+
+
 
 export default function PopUpEvent(props: {
   setIsPopUp: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,7 +12,10 @@ export default function PopUpEvent(props: {
   const url = process.env.NEXT_PUBLIC_API_URL;
   const [event, setEvent] = useState<Event>();
 
-  let imageUrl = 'bg-[url(/cageMMA.png)]';
+
+     
+    
+  let imageUrl = "bg-[url(/cageMMA.png)]";
   switch (event?.sport) {
     case 'jjb':
       imageUrl = 'bg-[url(/TTjjb.png)]';
@@ -27,40 +32,32 @@ export default function PopUpEvent(props: {
   }
 
   useEffect(() => {
-    fetch(`${url}events/${props.token}`)
-      .then((response) => response.json())
-      .then((data) => setEvent(data.data));
-  }, []);
+    fetch(`${url}events/${props.token}`).then(response => response.json())
+      .then(data => setEvent(data.data))
+  }, [])
   console.log(imageUrl);
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-background/80 flex flex-col justify-center items-center">
-      <div
-        className={`w-150 h-100 ${imageUrl} bg-cover flex flex-col justify-between rounded-xl relative p-5 items-center`}>
+      <div className={`w-150 h-100 ${imageUrl} bg-cover flex flex-col justify-between rounded-xl relative p-5 items-center`}>
+
+
         <h1 className="text-wight text-2xl text-center border-5 w-[40%]">EVENT</h1>
-        {event && (
+        {event &&
           <div>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold">
-              {' '}
-              {event.clubName}{' '}
-            </h2>
+            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.clubName} </h2>
             <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.sport} </h2>
             <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.level}</h2>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold">
-              {' '}
-              {dateToString(event.date)}
-            </h2>
+            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {DateToString(new Date(event.date))}</h2>
           </div>
-        )}
+        }
 
-        <Button
-          variant={ButtonVariant.Refuse}
-          className="text-xs text-wight cursor-pointer absolute top-3 right-3"
-          onClick={() => props.setIsPopUp(false)}>
-          X
-        </Button>
 
-        <div className="flex justify-center items-end p-4">
-          <Button variant={ButtonVariant.Accept}>Join</Button>
+
+        <Button variant={ButtonVariant.Refuse} className="text-xs text-wight cursor-pointer absolute top-3 right-3" onClick={() => props.setIsPopUp(false)}>X</Button>
+
+        <div className="flex justify-center space-x-4 items-end p-1">
+          <Button className="w-30" onClick={handleJoin} variant={ButtonVariant.Accept}>Join</Button>
+          <Button className="w-30" variant={ButtonVariant.Refuse}>More info</Button>
         </div>
       </div>
       {/* <p className="text-black ">The "Date" at the "Club" join for the "sport" fight.</p> */}
