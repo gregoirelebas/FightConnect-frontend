125cbe4931c7f63025ba68c083366056a2b0a648
kalifa8869 <kalifa.seydi@live.fr>
2025-12-16 15:27:24 +0100

    change

--- app/events/PopUpEvent.tsx
+++ app/events/PopUpEvent.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import Button, { ButtonVariant } from "../...components/Button";
 import { Event } from "../...types/event";
-import { DateToString } from "../...helpers/date";
+import { dateToString } from "../...helpers/date";
+import { LevelToString } from "../...helpers/enum";
+import { SportToString } from "../...helpers/enum";
+import { getCookie } from "../...helpers/cookies";
+import Cookies from "../...types/cookies";
 
 
 
@@ -11,21 +15,23 @@ export default function PopUpEvent(props: {
 }) {
   const url = process.env.NEXT_PUBLIC_API_URL;
   const [event, setEvent] = useState<Event>();
+  const [fighterToken, setFighterToken] = useState('')
 
 
-     
-    
-  let imageUrl = "bg-[url(/cageMMA.png)]";
+  let imageUrl = 'bg-[url(/cageMMA.png)]';
   switch (event?.sport) {
     case 'jjb':
       imageUrl = 'bg-[url(/TTjjb.png)]';
       break;
     case 'kickBoxing':
       imageUrl = 'bg-[url(/ring.png)]';
+      break;
     case 'englishBoxing':
       imageUrl = 'bg-[url(/ring.png)]';
+      break;
     case 'muayThai':
       imageUrl = 'bg-[url(/ring.png)]';
+      break;
 
     default:
       break;
@@ -34,26 +40,50 @@ export default function PopUpEvent(props: {
   useEffect(() => {
     fetch(`${url}events/${props.token}`).then(response => response.json())
       .then(data => setEvent(data.data))
+    getCookie(Cookies.token).then(cookie => {
+      if (cookie) {
+        setFighterToken(cookie)
+      }
+    })
+    
+
   }, [])
+
+  const handleJoin = () => {
+    try {
+      fetch(`${url}events/join`,
+        {
+          method: 'PUT',
+          body: JSON.stringify({ fighterToken, eventToken: props.token }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+    } catch (error) {
+      console.log(error)
+    }
+
+
+
+  }
+
   console.log(imageUrl);
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-background/80 flex flex-col justify-center items-center">
-      <div className={`w-150 h-100 ${imageUrl} bg-cover flex flex-col justify-between rounded-xl relative p-5 items-center`}>
+      <div className={`w-150 h-100 ${imageUrl} bg-cover flex flex-col justify-between border border-accent rounded-xl relative p-5 items-center`}>
 
 
         <h1 className="text-wight text-2xl text-center border-5 w-[40%]">EVENT</h1>
         {event &&
-          <div>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.clubName} </h2>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.sport} </h2>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {event.level}</h2>
-            <h2 className="text-wight text-2xl flex-auto text-center font-bold"> {DateToString(new Date(event.date))}</h2>
+          <div className="h-55">
+            <h2 className="text-wight  text-2xl flex-auto text-center font-bold"> {event.clubName} </h2>
+            <h2 className="text-wight  text-2xl flex-auto text-center font-bold"> {SportToString(event.sport)} </h2>
+            <h2 className="text-wight  text-2xl flex-auto text-center font-bold"> {LevelToString(event.level)}</h2>
+            <h2 className="text-wight  text-2xl flex-auto text-center font-bold"> {dateToString(new Date(event.date))}</h2>
           </div>
-        }
+        };
 
 
 
-        <Button variant={ButtonVariant.Refuse} className="text-xs text-wight cursor-pointer absolute top-3 right-3" onClick={() => props.setIsPopUp(false)}>X</Button>
+        <Button variant={ButtonVariant.Ternary} className="text-xs text-wight cursor-pointer absolute top-3 right-3" onClick={() => props.setIsPopUp(false)}>X</Button>
 
         <div className="flex justify-center space-x-4 items-end p-1">
           <Button className="w-30" onClick={handleJoin} variant={ButtonVariant.Accept}>Join</Button>
