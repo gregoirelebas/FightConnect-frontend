02df0f91e0ecfd2e363afcee17a526ced975da0e
sylvain.b <sylvain.berthere@gmail.com>
2025-12-17 11:04:53 +0100

    Added the city, changed the promoterID and design of create Event

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -12,7 +12,7 @@ import { Event } from "@/app/...types/event";
 import SportDropdown from "@/app/...UI/SportDropdown";
 import { useRouter } from "next/navigation";
 import { getCookie } from "@/app/...helpers/cookies";
-import { getFormatedDate, getFormatedDateNewEvent } from "@/app/...helpers/date";
+import { getFormatedDateNewEvent } from "@/app/...helpers/date";
 
 export default function NewEvent() {
   const router = useRouter();
@@ -21,22 +21,25 @@ export default function NewEvent() {
   const [level, setLevel] = useState<Level>(Level.Amateur);
   const [date, setDate] = useState<Date>(new Date());
   const [club, setClub] = useState<string>("");
+  const [city, setCity] = useState<string>("");
   const [sport, setSport] = useState<Sport>(Sport.EnglishBoxing);
   const [experience, setExperience] = useState<Experience>(Experience.Zero);
   const [weight, setWeight] = useState<Weight>(Weight.FiftyTwoFiftySeven);
   const [description, setDescription] = useState<string>("");
+  const [token, setToken] = useState<string>("");
 
   const createEvent = async () => {
     const event: Event = {
       level: level,
+      city: city,
       sport: sport,
       clubName: club,
-      date: date,
+      date: date.toString(),
       experience: experience,
       weight: weight,
       name: eventName,
       description: description,
-      promoterToken: "Ngt5y-2O32s1T9Y44AJJufXzhfM2bokp",
+      promoterToken: token,
       fighters: [],
       token: "",
     };
@@ -63,17 +66,19 @@ export default function NewEvent() {
     const fetchCookie = async () => {
       const result = await getCookie("date");
       if (result) {
-      setDate(new Date(result));
+        setDate(new Date(result));
+      }
+      const token = await getCookie("token");
+      if (token) {
+        setToken(token);
       }
     };
     fetchCookie();
   }, []);
 
-
-
   return (
     <div className="flex flex-col items-center h-[calc(100vh-80px)] bg-[url(/createRing.png)] bg-center bg-cover text-white font-sans ">
-      <div className="h-full w-1/2 bg-foreground roun flex flex-col font-bold justify-around items-center">
+      <div className="h-full w-1/2 bg-foreground/98 flex flex-col font-bold justify-around items-center">
         <div className="flex flex-row h-1/6 w-full items-center justify-center border-2 border-l-0 border-r-0 border-t-0">
           <div className="flex flex-col mr-50">
             <h1>Create an Event</h1>
@@ -86,10 +91,10 @@ export default function NewEvent() {
             </Button>
           </Link>
         </div>
-        <div className="flex flex-col justify-around items-center h-2/5">
-          <div className="h-20 flex flex-col justify-between">
+        <div className="flex flex-col justify-around items-center h-3/6">
+          <div className="h-3/10 w-3/5 flex flex-col justify-center">
             <span>Event Type :</span>
-            <div className="flex flex-row w-110 justify-between">
+            <div className="flex flex-row mt-3 justify-between">
               <div className="h-12 w-50 bg-foreground border-2 rounded-2xl border-accent flex items-center justify-center">
                 <RadioButton
                   name="level"
@@ -109,7 +114,7 @@ export default function NewEvent() {
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap w-200 h-7/10 mt-10 justify-around">
+          <div className="grid grid-cols-3 grid-rows-3 w-200 h-7/10">
             <Input
               label="Name of Event"
               placeholder="Event Name..."
@@ -118,6 +123,7 @@ export default function NewEvent() {
               onChange={(value) => setEventName(String(value))}
             />
             <Input
+              className="w-61"
               label="Date of Event"
               placeholder="Enter Date Event"
               type="date"
@@ -131,14 +137,21 @@ export default function NewEvent() {
               value={club}
               onChange={(value) => setClub(String(value))}
             />
+            <Input
+              label="City"
+              placeholder="City name"
+              type="text"
+              value={city}
+              onChange={(value) => setCity(String(value))}
+            />
             <div className="h-20 flex flex-col justify-between">
               <span>Sports :</span>
-              <SportDropdown className="w-50" onChange={(value) => setSport(value as Sport)} />
+              <SportDropdown className="w-61" onChange={(value) => setSport(value as Sport)} />
             </div>
             <div className="h-20 flex flex-col justify-between">
               <span>Experience :</span>
               <Dropdown
-                className="w-50"
+                className="w-61"
                 options={[
                   { value: Experience.Zero, label: "0" },
                   { value: Experience.OneThree, label: "1-3" },
@@ -157,7 +170,7 @@ export default function NewEvent() {
             <div className="h-20 flex flex-col justify-between">
               <span>Category of Weight :</span>
               <Dropdown
-                className="w-50"
+                className="w-61"
                 options={[
                   { value: Weight.FiftyTwoFiftySeven, label: "52-57" },
                   { value: Weight.FiftySevenSixtyOne, label: "57-61" },
@@ -173,6 +186,7 @@ export default function NewEvent() {
             </div>
           </div>
         </div>
+        <div className="h-2/6 flex flex-col items-center">
         <div className="w-200">
           <TextArea
             label="Event Description"
@@ -185,6 +199,7 @@ export default function NewEvent() {
         <Button className="w-100" variant={ButtonVariant.Accept} onClick={() => createEvent()}>
           Create Event
         </Button>
+        </div>
       </div>
     </div>
   );
